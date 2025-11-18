18b2deedf31a4b36330433b776966febe8f6ad0b
vfxu-nor <ostebaget@gmail.com>
2025-11-18 21:27:51 +0100

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,11 @@
+local repo = "https://raw.githubusercontent.com/vfxu-nor/m0th-v1/main/"
+
+loadstring(game:HttpGet(repo.."ui.lua"))()  
+task.wait(0.2)
+
+loadstring(game:HttpGet(repo.."main.lua"))()
+loadstring(game:HttpGet(repo.."box.lua"))()
+loadstring(game:HttpGet(repo.."lines.lua"))()
+loadstring(game:HttpGet(repo.."aim.lua"))()
+
+print("m0th v1 loaded – 5-file version")
